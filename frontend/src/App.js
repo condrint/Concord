b328eb29a2f861a3c7e3359111975668e65bfd69
Josh Coyco <jac6308@gmail.com>
2018-11-02 12:12:18 -0700

    Trying arrow functions to pass props

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,16 +12,17 @@ class App extends Component {
     this.handleLoginSubmit = this.handleLoginSubmit.bind(this);
   }
   
-    handleLoginChange = event => {
-        this.setState({
-            [event.target.id]: event.target.value
-        });
-    }
+  handleLoginChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  }
 
-    handleLoginSubmit = event => {
-        event.preventDefault();
-        console.Log("hi");
-    }
+  handleLoginSubmit(event) {
+    console.Log("hi");
+    event.preventDefault();
+  }
+
   render() {
     let loggedIn = this.state.isLoggedIn;
     let username = this.state.username;
--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -12,11 +12,11 @@ import React, { Component } from 'react';
     {
         return (
           <div>
-            <form id = "loginForm" onSubmit={this.props.loginSubmit}>
+            <form id = "loginForm" onSubmit={()=>this.props.loginSubmit}>
               Username <br/>
-              <input id="username" type="text" onChange={this.props.loginChange}/> <br/>
+              <input id="username" type="text" onChange={()=>this.props.loginChange} /> <br/>
               Password <br/>
-              <input id="password" type="text"  onChange={this.props.loginChange}/>
+              <input id="password" type="text" onChange={()=>this.props.loginChange} />
               <br/>
               <button id="loginButton" type="submit">Login</button>
             </form>
