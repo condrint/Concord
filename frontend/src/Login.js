4935a3db1cef5dc05d9af5e16a3d07f9e171553f
condrint <condrint@gmail.com>
2018-11-02 17:36:30 -0700

    removed value tags for buttons

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -11,25 +11,25 @@ import React, { Component } from 'react';
     render() {
       return (
         <div>
-          <div>
+          <div id="loginWrapper">
             <form id = "loginForm" onSubmit={this.props.loginSubmit}>
               Username <br/>
               <input id="loginUsernameInput" type="text" onChange={this.props.loginChange} value={this.props.loginUsernameInput}/> <br/>
               Password <br/>
               <input id="loginPasswordInput" type="text" onChange={this.props.loginChange} value={this.props.loginPasswordInput}/>
               <br/>
-              <button id="loginButton" type="submit" value="Submit">Login</button><br/>
+              <button id="loginButton" type="submit">Login</button>
             </form>
           </div>
           <br/>
-          <div>
+          <div id="registerWrapper">
             <form id = "registerForm" onSubmit={this.props.registerSubmit}>
               Username <br/>
               <input id="registerUsernameInput" type="text" onChange={this.props.registerChange} value={this.props.registerUsernameInput}/> <br/>
               Password <br/>
               <input id="registerPasswordInput" type="text" onChange={this.props.registerChange} value={this.props.registerPasswordInput}/>
               <br/>
-              <button id="loginButton" type="submit" value="Submit">Register</button>
+              <button id="registerButton" type="submit">Register</button>
             </form>
           </div>
         </div>
