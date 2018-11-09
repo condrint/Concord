62ae72074124d2e8d97a2c3239e6ab1e67a1a5e1
Josh Coyco <jac6308@gmail.com>
2018-11-09 11:31:25 -0800

    Now working Login and Register, will implement axios

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -10,31 +10,41 @@ import React, { Component } from 'react';
 
     render()
     {
+      if(this.props.form === "login")
+      {
         return (
           <div>
             <div>
               <form id = "loginForm" onSubmit={this.props.loginSubmit}>
                 Username <br/>
-                <input id="loginUsernameInput" type="text" onChange={this.props.loginChange} value={this.props.loginUsernameInput}/> <br/>
+                <input id="loginUsernameInput" type="text" onChange={this.props.change} value={this.props.loginUsernameInput}/> <br/>
                 Password <br/>
-                <input id="loginPasswordInput" type="text" onChange={this.props.loginChange} value={this.props.loginPasswordInput}/>
+                <input id="loginPasswordInput" type="password" onChange={this.props.change} value={this.props.loginPasswordInput}/>
                 <br/>
-                <button id="loginButton" type="submit" value="Submit">Login</button><br/>
+                <button id="loginButton" type="submit" value="Submit">Login</button>
+                <button id="changeForm" onClick={this.props.formChange} value="Submit" type="click">Click here to Register</button>
               </form>
             </div>
             <br/>
-            <div>
-            <form id = "registerForm" onSubmit={this.props.registerSubmit}>
-              Username <br/>
-              <input id="registerUsernameInput" type="text" onChange={this.props.registerChange} value={this.props.registerUsernameInput}/> <br/>
-              Password <br/>
-              <input id="registerPasswordInput" type="text" onChange={this.props.registerChange} value={this.props.registerPasswordInput}/>
-              <br/>
-              <button id="loginButton" type="submit" value="Submit">Register</button>
-            </form>
-            </div>
           </div>
         );
+      }
+      else if(this.props.form === "register")
+      {
+        return (
+        <div>
+            <form id = "registerForm" onSubmit={this.props.registerSubmit}>
+              Username <br/>
+              <input id="registerUsernameInput" type="text" onChange={this.props.change} value={this.props.registerUsernameInput}/> <br/>
+              Password <br/>
+              <input id="registerPasswordInput" type="password" onChange={this.props.change} value={this.props.registerPasswordInput}/>
+              <br/>
+              <button id="loginButton" type="submit" value="Submit">Register</button>
+              <button id="changeForm" onClick={this.props.formChange} value="Submit" type="click">Click here to Login</button>
+            </form>
+          </div>
+        );
+      }
     }
  }
 
