c882230bb4cca9e3db2a0dcfd75634d1585d9e2e
condrint <condrint@gmail.com>
2019-01-19 17:39:56 -0800

    additional message boilerplate

--- backend/controllers/message_controller.js
+++ backend/controllers/message_controller.js
@@ -2,6 +2,12 @@ var Message = require('../models/message.js');
 
 const messageController = {};
 
+messageController.addMessage = async (req, res) => {
 
+}
+
+messageController.getMessage = async (req, res) => {
+
+}
 
 module.exports = userController;
