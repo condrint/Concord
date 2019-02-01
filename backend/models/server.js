21ebba7cb762e018857e2ce78b6fab1635194374
Edward <etbte@yahoo.com>
2019-02-01 12:36:54 -0800

    change ChatId to MessageId in serverschema

--- backend/models/server.js
+++ backend/models/server.js
@@ -5,21 +5,8 @@ var uniqueValidator = require('mongoose-unique-validator');
 var serverSchema = new Schema({
     serverName: String,
     ownerName: String,
-<<<<<<< HEAD
-    members: [{
-        memberId: String,
-        username: String,
-        chatId: String,
-    }],
-    chatHistory: [{
-        userName: String,
-        message: String,
-        time: { type : Date } //, default: Date.now }
-    }]
-=======
     members: [{memberName: String}],
-    chatId: String,
->>>>>>> 872bec8aa6eca054877793b560d8000d85c01ef4
+    messageId: String,
 })
 
 
