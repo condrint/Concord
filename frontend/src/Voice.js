570b42635e11b29c01a762f097b59badecc09df1
condrint <condrint@gmail.com>
2019-02-16 10:13:30 -0800

    stun

--- frontend/src/Voice.js
+++ frontend/src/Voice.js
@@ -10,7 +10,7 @@ class Voice extends Component {
     this.state = {
       peer: new Peer({ 
         channelName: this.props.callMessageId,
-        config: { iceServers: [{ urls: 'stun:stun.callwithus.com'}]},
+        config: { iceServers: [{ 'urls': 'stun:stun.l.google.com:19302'}]},
         initiator: this.props.isInitiator,
       }),
     }
